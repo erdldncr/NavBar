def4c16053b6931af0db0e36ed078b7926e90965
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-02-25 19:21:16 +0000

    project has been completedt

---
+++ app.js
@@ -0,0 +1,4 @@
+document.querySelector('.nav-toggle').addEventListener('click',function(){
+    console.log(document.querySelector('.links'))
+    document.querySelector('.links').classList.toggle('show-links')
+})
